9b62c97c1c9fe89abf36a5ebac5173b0cb62cb3e
James P. Torre, IV <torreaiworks01@gmail.com>
2015-06-29 17:27:49 +0000

    Small change.

--- kirkman.scm
+++ kirkman.scm
@@ -1,8 +1,11 @@
 ;;  kirkman.scm  jpt4  UTC20150628
 
+(load "miniKanren-with-symbolic-constraints/mk.scm")
+(load "miniKanren-with-symbolic-constraints/numbers.scm")
 
 ;;  produce set B of subsets of size 3, from set P of size v >= 3, such that
 ;;  no (i,j) pair /element-of/ PxP is present in more than one subset.
+#|
 (define (kirkmano p b)
   (fresh (ps cr)
     (crosspro p p cr)
@@ -10,15 +13,21 @@
 (conde
 [])))
 
+
 (define (crosspro m n o)
   (conde
-    [(nullo m) (nullo n) o]
+    [(nullo m) (nullo n) succeed]
     [(fresh (ma md na nd res)
        (== `(,ma . ,md) m) (== `(,na . ,nd) n)
        (crosspro-auxo ma n res)
+			 |#
 
 (define (crosspro-auxo s ls o)
   (conde
-    [
+    [(nullo ls) succeed]
+    [(fresh (lsa lsd res)
+       (== `(,lsa . ,lsd) ls)
+       (==  `((,s . ,lsa) . ,o) res)
+       (crosspro-auxo s lsd res))]))
   
     
