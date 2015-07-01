2f41cdf5701f186e4b2c47f8c92a18ae969af3ac
James P. Torre, IV <torreaiworks01@gmail.com>
2015-07-01 06:26:40 +0000

    More mk thrashing

--- kirkman.scm
+++ kirkman.scm
@@ -1,7 +1,9 @@
 ;;  kirkman.scm  jpt4  UTC20150628
+;;  Kirkman's Schoolgirl Puzzle
 
 (load "miniKanren-with-symbolic-constraints/mk.scm")
 (load "miniKanren-with-symbolic-constraints/numbers.scm")
+(load "prelude.scm")
 
 ;;  produce set B of subsets of size 3, from set P of size v >= 3, such that
 ;;  no (i,j) pair /element-of/ PxP is present in more than one subset.
@@ -14,21 +16,23 @@
 [])))
 |#
 
-(define (crosspro m n t o)
+(define (crosspro m n o)
   (conde
-    [(== '() m) (=/= '() n) (== t o)]
+    [(== '() m) (=/= '() n) (== '() o)]
     [(fresh (ma md na nd rest reso)
        (== `(,ma . ,md) m) (== `(,na . ,nd) n)
-       (crosspro-auxo ma n rest reso)
-       (crosspro md n rest reso))]))
+       (crosspro-auxo `(,ma) n rest)
+       (== `(,rest . ,reso) freso)
+       (flatteno freso o)
+       (crosspro md n reso))]))
 
-
-(define (crosspro-auxo s ls t o)
+(define (crosspro-auxo s ls o)
   (conde
-    [(=/= '() ls) (=/= '() t) (== t o)]
+    [(=/= '() s) (== '() ls) (== '() o)]
     [(fresh (sa lsa lsd res)
        (== `(,sa) s)
        (== `(,lsa . ,lsd) ls)
-       (==  `((,sa . ,lsa) . ,t) res)
-       (crosspro-auxo s lsd res o))]))
+       (==  `((,sa . ,lsa) . ,res) o)
+       (crosspro-auxo s lsd res))]))
   
+
--- prelude.scm
+++ prelude.scm
@@ -1,8 +1,8 @@
 ;;  prelude.scm  jpt4  UTC20150630
 ;;  basic mk idioms
 
-(define (null?o i)
-  (== '() i))
+(define (add1o i o)
+  (pluso i '(1) o))
 
 (define (caro i o)
   (fresh (ls)
@@ -12,11 +12,21 @@
   (fresh (ls)
     (== `(ls . o) i)))
 
-(define (add1o i o)
-  (pluso i '(1) o))
-
-(define (sub1o i o)
-  (add1o o i))
+(define (flatteno i o)      ;;  TODO: Need to fix conde overlap
+  (conde
+    [(null?o i) (null?o o)]
+    [(fresh (a d aa aa2 ad res)
+       (== `(,a . ,d) i)    ;;  i is a list
+       (=/= `(,aa . ()) a)  ;;  (car i) is not a list
+       (== `(,aa2 . ,ad) a)
+       (== aa2 aa)
+       (== `(,a . ,res) o)
+       (flatteno d res))]
+    [(fresh (a aa d res)
+       (== `(,a . ,d) i)
+       (== `(,aa . ()) a)   ;;  (car a) is a list
+       (== `(,a . ,res) o)
+       (flatteno d res))]))
 
 (define (lengtho ls len o)
   (conde
@@ -25,4 +35,12 @@
        (== `(,lsa . ,lsd) ls)
        (add1o len res)
        (lengtho lsd res o))]))
-       
+
+(define (non-null?o i)
+  (=/= '() i))
+
+(define (null?o i)
+  (== '() i))
+
+(define (sub1o i o)
+  (add1o o i))
