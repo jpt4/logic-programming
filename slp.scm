537c53b7a7c838e506015251ff7c84da00717c3d
James P. Torre, IV <torreaiworks01@gmail.com>
2017-09-14 12:19:51 +0000

    Naive solver workd.

--- slp.scm
+++ slp.scm
@@ -35,10 +35,11 @@
 ;;^^may be wrong, irrelevant
 
 ;Symmetry
-(define (sym r i o) ;rule r does not need to be passed quoted. (sym dneg 'x q) => (~ (~ x))
+;rule r does not need to be passed quoted. (sym dneg 'x q) => (~ (~ x))
+(define (sym r i o) 
   (r o i))
 
-;; Eight Assumed Equivalence Rules
+;; Eight Assumed Equivalence Rules and Duals
 
 ;Double Negation
 (define (dneg i o)
@@ -47,6 +48,7 @@
            (any-statement x)
            (== `(~ (~ ,x)) i)
            (== x o))]))
+(define (du-dneg i o) (sym dneg i o))
 ;Idempotency
 (define (idem i o)
   (conde
@@ -54,6 +56,7 @@
            (any-statement x)
            (== `(& ,x ,x) i)
            (== x o))]))
+(define (du-idem i o) (sym idem i o))
 ;Commutativity
 (define (comm i o)
   (conde
@@ -61,6 +64,7 @@
            (any-statement x) (any-statement y)
            (== `(& ,x ,y) i)
            (== `(& ,y ,x) o))]))
+(define (du-comm i o) (sym comm i o))
 ;Associativity
 (define (assoc i o)
   (conde
@@ -68,6 +72,7 @@
            (any-statement x) (any-statement y) (any-statement z)
            (== `(& ,x (& ,y ,z)) i)
            (== `(& (& ,x ,y) ,z) o))]))
+(define (du-assoc i o) (sym assoc i o))
 ;Absorption
 (define (absorp i o)
   (conde
@@ -75,6 +80,7 @@
            (any-statement x) (any-statement y)
            (== `(& ,x (// ,x ,y)) i)
            (== x o))]))
+(define (du-absorp i o) (sym absorp i o))
 ;Distributivity
 (define (distr i o)
   (conde
@@ -82,6 +88,7 @@
            (any-statement x) (any-statement y) (any-statement z)
            (== `(& ,x (// ,y ,z)) i)
            (== `(// (& ,x ,y) (& ,y ,z)) o))]))
+(define (du-distr i o) (sym distr i o))
 ;DeMorgan's Rule
 (define (dem i o)
   (conde
@@ -89,6 +96,7 @@
            (any-statement x) (any-statement y)
            (== `(~ (& ,x ,y)) i)
            (== `(& (~ ,x) (~ ,y)) o))]))
+(define (du-dem i o) (sym dem i o))
 ;Dichotomy
 (define (dichot i o)
   (conde
@@ -96,6 +104,7 @@
            (any-statement x) (any-statement y)
            (== `(// ,x (~ ,x)) i)
            (== `(// ,y (~ ,y)) o))]))
+(define (du-dichot i o) (sym dichot i o))
 
 ;; Syntactically mark statements as equivalent.
 (define (mk-eqv i o)
@@ -103,14 +112,53 @@
    [(eqvo i o) `(eqv i o)]))
 
 (define (eqvo i t o)
-  (fresh (x y new-exp res-sub-t sub-t sub-o)
+  (fresh (x y new-exp res-sub-t sub-t sub-o
+            s1 s2 res-sub-t-1 res-sub-t-2 sub-t-1 sub-t-2 sub-o-1 sub-o-2)
          (conde
+          ;trivial result
+          [(== i o) (== '() t)]
+          ;simple statement grounding
           [(simple-statement i) (== `((,i simp)) t) (== i o)]
-          [(dneg i x) (== `((,x dneg) . ,y) t) (eqvo x y o)]
+          ;eight equivalence rules - contraction
+          [(dneg i x) (== `((,x dneg) . ,y) t) 
+           #;(rule-result-cycle-check x t) (eqvo x y o)]
+          [(idem i x) (== `((,x idem) . ,y) t) (eqvo x y o)]
+          [(comm i x) (== `((,x comm) . ,y) t) (eqvo x y o)]
+          [(assoc i x) (== `((,x assoc) . ,y) t) (eqvo x y o)]
+          [(absorp i x) (== `((,x absorp) . ,y) t) (eqvo x y o)]          
+          [(distr i x) (== `((,x distr) . ,y) t) (eqvo x y o)]          
+          [(dem i x) (== `((,x dem) . ,y) t) (eqvo x y o)]          
+          [(dichot i x) (== `((,x dichot) . ,y) t) (eqvo x y o)]          
+          ;eight dual equivalence rules - expansion
+          [(du-dneg i x) (== `((,x du-sym-dneg) . ,y) t) (eqvo x y o)]
+          [(du-idem i x) (== `((,x du-idem) . ,y) t) (eqvo x y o)]
+          [(du-comm i x) (== `((,x du-comm) . ,y) t) (eqvo x y o)]
+          [(du-assoc i x) (== `((,x du-assoc) . ,y) t) (eqvo x y o)]
+          [(du-absorp i x) (== `((,x du-absorp) . ,y) t) (eqvo x y o)]
+          [(du-distr i x) (== `((,x du-distr) . ,y) t) (eqvo x y o)]          
+          [(du-dem i x) (== `((,x du-dem) . ,y) t) (eqvo x y o)]          
+          [(du-dichot i x) (== `((,x du-dichot) . ,y) t) (eqvo x y o)]
+          ;Substitution Principle - compound decomposition
           [(mk-not x i) (== `((,x not-comp) . ,res-sub-t) sub-t) 
            (eqvo x res-sub-t sub-o)
            (== `((sub-trace ,sub-t) . ,y) t)
            (== `(~ ,sub-o) new-exp)
+           (eqvo new-exp y o)]
+          [(mk-and s1 s2 i) 
+           (== `((,s1 and-comp-1) . ,res-sub-t-1) sub-t-1) 
+           (== `((,s2 and-comp-2) . ,res-sub-t-2) sub-t-2)            
+           (eqvo s1 res-sub-t-1 sub-o-1)
+           (eqvo s2 res-sub-t-2 sub-o-2)
+           (== `((sub-trace ,sub-t-1) (sub-trace ,sub-t-2) . ,y) t)
+           (== `(& ,sub-o-1 ,sub-o-2) new-exp)
+           (eqvo new-exp y o)]
+          [(mk-or s1 s2 i) 
+           (== `((,s1 or-comp-1) . ,res-sub-t-1) sub-t-1) 
+           (== `((,s2 or-comp-2) . ,res-sub-t-2) sub-t-2)            
+           (eqvo s1 res-sub-t-1 sub-o-1)
+           (eqvo s2 res-sub-t-2 sub-o-2)
+           (== `((sub-trace ,sub-t-1) (sub-trace ,sub-t-2) . ,y) t)
+           (== `(// ,sub-o-1 ,sub-o-2) new-exp)
            (eqvo new-exp y o)]
           )))
 
